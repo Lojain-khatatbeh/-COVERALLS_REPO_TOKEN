3965b8fb6c915a12078e5a286b46db41d02022ce
Lojain-khatatbeh <lojainkatatbh@gmail.com>
2025-05-17 20:56:38 +0100

    Create لباf.js

---
+++ لباf.js
@@ -0,0 +1,11 @@
+// دالة تحسب مجموع رقمين
+function sum(a, b) {
+  return a + b;
+}
+
+// استخدام الدالة
+const num1 = 5;
+const num2 = 7;
+const result = sum(num1, num2);
+
+console.log("المجموع هو:", result);
